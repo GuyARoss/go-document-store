4c24a526151bdc12ca9bedc6b21ff8d3e53bba1b
GuyARoss <guyallenross@gmail.com>
2019-07-09 17:17:29 -0400

    store_utilities - wip.

--- src/store/store_utilitiles.go
+++ src/store/store_utilitiles.go
@@ -1,9 +1,36 @@
 package store
 
-func updateFromIo(string name) *byte[] {
+import (
+	"os"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+)
 
+func updateFromIo(name string) *[]byte {
+	data := &[]byte{}
+
+	pname := fmt.Sprintf("/tmp/%s", name)
+	if _, err := os.Stat(pname); os.IsNotExist(err) {
+		os.Create(pname)
+		return data
+	}
+
+	file, _ := os.Open(pname)
+    defer file.Close()
+
+	b, _ := ioutil.ReadAll(file)
+	return &b
 }
 
-func copyToIo(string name, stream *byte[]) {
+func copyToIo(name string, stream *[]byte) {
+	pname := fmt.Sprintf("../tmp/%s", name)
+	absFilePath, _ := filepath.Abs(pname)
 
+	if _, err := os.Stat(absFilePath); os.IsNotExist(err) {
+		os.Create(absFilePath)
+	}
+
+	fmt.Println(absFilePath)
+	ioutil.WriteFile(absFilePath, *stream, 0644)
 }
