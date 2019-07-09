d8a71f9bc402b43938a17063ec906a14c27334d2
GuyARoss <guyallenross@gmail.com>
2019-07-09 17:16:56 -0400

    store_manager - added logic for init store - added logic for creating instance - added logic for getting instance.

--- src/store_manager.go
+++ src/store_manager.go
@@ -1,27 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"manager/store"
+	"path/filepath"
 )
 
-type SessionSettings type {
-	Instances 	map[string]manger.Instance
+type SessionSettings struct {
+	Instances 	map[string]*store.Instance
 }
 
-func (session *SessionSettings) GetInstance(instanceName string) {
+func (session *SessionSettings) GetInstance(instanceName string) *store.Instance {
 	return session.Instances[instanceName]
 }
 
-func Initialize() {
-	instances := make(map[string]manager.Instance)
+func (session *SessionSettings) CreateInstance(instanceName string, data *[]byte) {
+	createdInstance := store.CreateInstance(instanceName)
+	fmt.Print("passed!02")
+	createdInstance.Update(data)
 
-	files, err := ioutil.ReadDir("./")
-	if err != nil {
-		log.Fatal(err)
-	}
+	session.Instances[instanceName] = createdInstance
+}
+
+func Init() *SessionSettings {
+	instances := make(map[string]*store.Instance)
+
+	absFilePath, _ := filepath.Abs("../temp/")
+	files, _ := ioutil.ReadDir(absFilePath)
 
 	for _, f := range files {
-		fmt.Println(f.Name())
+		instances[f.Name()] = store.CreateInstance(f.Name())
+	}
+
+	return &SessionSettings{
+		Instances: instances,
 	}
 }
