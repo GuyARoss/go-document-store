613304156e1eaccb0024b721151443bbd66e879f
GuyARoss <guyallenross@gmail.com>
2019-07-10 11:22:12 -0400

    store_utilities - added rest of functionality for updateFromIo - updated paths

--- src/store/store_utilitiles.go
+++ src/store/store_utilitiles.go
@@ -9,24 +9,19 @@ import (
 )
 
 func updateFromIo(name string) *[]byte {
-	data := &[]byte{}
-
-	pname := fmt.Sprintf("/tmp/%s", name)
-	if _, err := os.Stat(pname); os.IsNotExist(err) {
-		os.Create(pname)
-		return data
-	}
+	pname := fmt.Sprintf("tmp/%s", name)
 
 	file, _ := os.Open(pname)
     defer file.Close()
 
 	b, _ := ioutil.ReadAll(file)
+	fmt.Println(string(b))
 	return &b
 }
 
 func copyToIo(name string, stream *[]byte) {
-	pathname := fmt.Sprintf("temp/%s", name)
-	os.MkdirAll("temp/", os.ModePerm)
+	pathname := fmt.Sprintf("tmp/%s", name)
+	os.MkdirAll("tmp/", os.ModePerm)
 	os.Create(pathname)
 
 	err := ioutil.WriteFile(pathname, *stream, 0644)
